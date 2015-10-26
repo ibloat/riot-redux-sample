1201571db9a710fc1c721635a93e35c9ec0855d2
ibloat <git@iblo.at>
2015-10-26 16:13:57 +0900

    add test stub

---
+++ test/index.js
@@ -0,0 +1,8 @@
+import test from 'tape'
+//import tag from '../src/code/tags/app.tag'
+
+test('A passing test', (assert) => {
+  assert.pass('This test will pass. Good job everyone.')
+
+  assert.end()
+})
